793ce9e1980c62bfd35c8b0ed8e8a068b1689bea
Miroslav Stoev <soboko@abv.bg>
2024-09-16 14:35:24 +0300

    Update 04-nameToUpperCase.js

--- 04-nameToUpperCase.js
+++ 04-nameToUpperCase.js
@@ -2,9 +2,10 @@ var text = prompt("What is your name?");
 var firstChar = text.slice(0, 1);
 var firstCharToUpperCase = firstChar.toUpperCase();
 var remaininCharacters = text.slice(1, text.length); 
+var remaininCharactersToLowerCase = remaininCharacters.toLowerCase();
+var actualName = firstCharToUpperCase + remaininCharactersToLowerCase;
 
-
-alert("Hello, " + firstCharToUpperCase + remaininCharacters + ".");
+alert("Hello, " + actualName + ".");
 
 
 
