500f7a14394b7b2853b7b99bc67b2a5398d6a5e0
Miroslav Stoev <soboko@abv.bg>
2024-09-16 17:12:59 +0300

    Update moveRobotEasy.js

--- moveRobotEasy.js
+++ moveRobotEasy.js
@@ -1,9 +1,18 @@
-var text = prompt("Write your text here:");
-var bouderOne = prompt("Write your start bounder here:");
-var bouderTwo = prompt("Write your end bounder here:");
-var boundingTexth = text.slice(bouderOne, bouderTwo);
+function main(){
+  
+  moveFourTimes();
+  turnLeft();
+  moveFourTimes();
+  
+}
 
-alert("You have written " + text + ". " + "Your text after boundering is : " + boundingTexth + ".");
+  function moveFourTimes(){
+   move();
+   move();
+   move();
+   move();
+}
+
 
 
 
