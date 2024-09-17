6ae56a61c93ee23283aa5095aca4c22ffc744c9f
Miroslav Stoev <soboko@abv.bg>
2024-09-17 17:33:36 +0300

    Update loveScoreCalculator.js

--- loveScoreCalculator.js
+++ loveScoreCalculator.js
@@ -4,8 +4,19 @@ prompt("Whats is their name?");
 var  loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
-alert("Your lovescore is: " + loveScore + "%");
+if(loveScore >= 80) {
+    alert("Your lovescore is: " + loveScore + "%." + " You are made for each other!");
+}
 
+if(loveScore >= 30 && loveScore < 80) {
+        
+        alert("Your lovescore is: " + loveScore + "%");
+    }
+
+if(loveScore < 30) {
+     
+    alert("Your lovescore is: " + loveScore + "%." + " You are not made for each other!");
+}
 
 
 
