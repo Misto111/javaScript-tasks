f365ae45fa82862e33d8c0fab9bc8e888d53c2d7
Miroslav Stoev <soboko@abv.bg>
2024-09-18 19:42:35 +0300

    Update bottlesChallenge.js

--- bottlesChallenge.js
+++ bottlesChallenge.js
@@ -1,9 +1,25 @@
-var text = prompt("Write your text here:");
-var bouderOne = prompt("Write your start bounder here:");
-var bouderTwo = prompt("Write your end bounder here:");
-var boundingTexth = text.slice(bouderOne, bouderTwo);
+var count = 100;
 
-alert("You have written " + text + ". " + "Your text after boundering is : " + boundingTexth + ".");
+function buyBeer() {
+
+    while(count >= 0) {
+
+        if(count === 0) { 
+          console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store an buy some more, 99 bottles of beer on the wall");
+            
+        }else if(count === 1) {
+            console.log(count + " " + "bottles of beer on the wall, " + (count) + " " + "bottles of beer." + " " + "Take 1 down, pass it around," + " " + " no more bottles of beer on the wall.");
+            
+        } else {
+            console.log(count + " " + "bottles of beer on the wall, " + (count) + " " + "bottles of beer." + " " + "Take 1 down, pass it around," + " " + (count - 1) + " " + "bottles of beer on the wall.");
+        }
+    
+    count--;
+
+}
+
+
+}
 
 
 
