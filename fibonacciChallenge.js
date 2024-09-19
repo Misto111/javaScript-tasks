a547ab92564fd2357a3579db539564ea5eb92d1e
Miroslav Stoev <soboko@abv.bg>
2024-09-19 09:34:41 +0300

    Add files via upload

---
+++ fibonacciChallenge.js
@@ -0,0 +1,12 @@
+var text = prompt("Write your text here:");
+var bouderOne = prompt("Write your start bounder here:");
+var bouderTwo = prompt("Write your end bounder here:");
+var boundingTexth = text.slice(bouderOne, bouderTwo);
+
+alert("You have written " + text + ". " + "Your text after boundering is : " + boundingTexth + ".");
+
+
+
+
+
+
