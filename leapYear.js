647143780641497c34067a8e3b7c6a8b24e5cc08
Miroslav Stoev <soboko@abv.bg>
2025-09-28 09:10:09 +0300

    Update leapYear.js

--- leapYear.js
+++ leapYear.js
@@ -1,8 +1,8 @@
 function isLeap(year) {
 
-    if(year % 4=== 0) {
+    if(year % 4 === 0) {
         if(year % 100 === 0) {
-            if(year % 100 === 0) {
+            if(year % 400 === 0) {
                 return "Leap year.";
             } else {
                 return "Not leap year.";
