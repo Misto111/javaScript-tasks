668edb22530ca2274d39e2c55d7191927e86046b
Miroslav Stoev <soboko@abv.bg>
2025-09-28 09:48:42 +0300

    Update fibonacciChallenge.js

--- fibonacciChallenge.js
+++ fibonacciChallenge.js
@@ -10,7 +10,7 @@ function fibonacciGenerator(n) {
         
     }else {
         output = [0, 1];
-
+        
         for (var i = 2; i < n; i ++) {
             output.push(output[output.length - 2] + 
             output[output.length -1]);
